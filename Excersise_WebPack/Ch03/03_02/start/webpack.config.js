0ae1d42bf8f469e6f4bc8045580cbde379b52d29
Oscar Hermawan <Oscar_H261@mitrais.com>
2019-11-14 17:05:22 +0800

    Chap-03_02-Load_image_with_webpack-Done

--- Excersise_WebPack/Ch03/03_02/start/webpack.config.js
+++ Excersise_WebPack/Ch03/03_02/start/webpack.config.js
@@ -24,6 +24,12 @@ module.exports = {
                     {loader: 'style-loader'},
                     {loader: 'css-loader'}
                 ]
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    {loader: 'url-loader'}
+                ]
             }
         ]
     }
